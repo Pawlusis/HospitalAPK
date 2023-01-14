882ea4638614f3532f18aca34ce27b010f9aa3c8
Pawlusis <pawlusis12@gmail.com>
2023-01-14 20:08:49 +0100

    update models.py

--- Patient/admin.py
+++ Patient/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Patients
+from .models import *
 
 # Register your models here.
 admin.site.register(Patients)
+admin.site.register(HospitalBed)
+admin.site.register(HospitalRoom)
+admin.site.register(HospitalWard)
--- Patient/models.py
+++ Patient/models.py
@@ -7,8 +7,32 @@ class Patients(models.Model):
     lastname_patient = models.CharField(max_length=64)
     birthdate_patient = models.DateField()
     admittingtime_patient = models.DateTimeField()
-    dischargetime_patient = models.DateTimeField(default='',blank=True,null=True)
+    dischargetime_patient = models.DateTimeField(default='', blank=True, null=True)
+    decribe_patient = models.TextField()
+
 
     def __str__(self):
         return self.firstname_patient + ' ' + self.lastname_patient
+
+class HospitalBed(models.Model):
+    number_of_bed = models.IntegerField()
+    type_of_bed = models.CharField(max_length=20)
+
+
+class HospitalRoom(models.Model):
+    number_of_room = models.IntegerField()
+    type_of_room = models.CharField(max_length=20)
+    count_of_beds = models.IntegerField()
+
+    def __str__(self):
+        return self.number_of_room
+
+
+class HospitalWard(models.Model):
+    name_of_ward = models.CharField(max_length=64)
+    count_of_rooms = models.IntegerField()
+
+    def __str__(self):
+        return self.name_of_ward
+
 
